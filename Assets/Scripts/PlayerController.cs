7b327915066f82e5b9c2362f1d3b56303c42fd71
jose.paulo <jose.paulo@ceuma.br>
2023-02-05 11:31:42 -0300

    nao atravessar a parede

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,30 +30,34 @@ public class PlayerController : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.UpArrow)){
                 input.x = 1;
                 input.y = 1;
-                refAnim.SetTrigger("go");
-                ToggleStance();
             }
             if(Input.GetKeyDown(KeyCode.RightArrow)){
                 input.x = 1;
                 input.y = -1;
-                refAnim.SetTrigger("go");
-                ToggleStance();
             }
             if(Input.GetKeyDown(KeyCode.LeftArrow)){
                 input.x = -1;
                 input.y = 1;
-                refAnim.SetTrigger("go");
-                ToggleStance();
             }
             if(Input.GetKeyDown(KeyCode.DownArrow)){
                 input.x = -1;
                 input.y = -1;
-                refAnim.SetTrigger("go");
-                ToggleStance();
             }
         }
 
-        transform.Translate(input.x*step, input.y*step, 0);
+        Vector2 position_pos_move = new Vector2(transform.position.x+input.x, transform.position.y+input.y);
+
+        if(
+            input.x!=0 &&
+            position_pos_move.x < 4 &&
+            position_pos_move.x > -4 &&
+            position_pos_move.y < 4.5f &&
+            position_pos_move.y > -3
+        ){
+            transform.Translate(input.x*step, input.y*step, 0);
+            refAnim.SetTrigger("go");
+            ToggleStance();
+        }
     }
 
     void ToggleStance(){
