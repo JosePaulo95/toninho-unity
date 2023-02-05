25620a46b97818f20dffe328db70d1a395ad47df
Jorge Henrique <jorge.henrique@comvanilla.com.br>
2023-02-04 21:12:35 -0300

    Implementacao do Inimigo

---
+++ .history/Assets/Scripts/Enemy_20230204204443.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Components")]
+    private PlayerController player;
+    
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.layer == 7)
+        {
+            player.AttackPlayer();
+        }
+    }
+}
